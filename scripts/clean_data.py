a63a5006cf53078cc6e02cccba70a7cd02a7813b
pixel-echo <tara.chakkithara@icloud.com>
2024-11-22 21:56:37 -0500

    cleaned data to look at neuston net samples

--- scripts/clean_data.py
+++ scripts/clean_data.py
@@ -2,7 +2,7 @@ import geopandas as gpd
 import pandas as pd
 
 data = gpd.read_file('../data/raw_data/raw_data.geojson')
-data = data[['OCEANS', 'SAMPMETHOD', 'MEASUREMEN', 'UNIT', 'Latitude', 'Longitude', 'Date', 'geometry']]
+data = data[data['SAMPMETHOD'] == 'Neuston net']
+data = data[['OCEANS', 'MEASUREMEN', 'Latitude', 'Longitude', 'Date', 'geometry']]
 data = data.rename(columns = {'MEASUREMEN' : 'Measurement', 'OCEANS': 'Ocean'})
-data = data[data['UNIT'] == 'pieces/m3']
 data.to_csv('../data/clean_data/clean_data.csv')
